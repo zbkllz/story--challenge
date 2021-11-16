811f772ee6fd60cce87338386875dd6426c50c67
Larissa <larissasousa.wrks@gmail.com>
2021-11-16 13:54:01 -0400

    :poop:

--- App.tsx
+++ App.tsx
@@ -1,16 +1,44 @@
-import React, {useState} from 'react';
-import {Text,SafeAreaView, View, TextInput, TouchableOpacity  } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import {styles} from './styles'
+import React, { useState } from 'react'
+import {
+  TextInput,
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity
+} from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { styles } from './styles'
+
 
 
 export default function App() {
+  const [text, setText] = useState('')
+
+  function buttonPressed(text: any) {
+    return text
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style= 'dark' />
-      <Text>Insert your name</Text>
+      <StatusBar style="dark" />
+
+      <TextInput
+        style={styles.txtname}
+        placeholder="Insert your name here"
+        value={text}
+        onChangeText={text => setText(text)}
+      />
+
+      <TouchableOpacity style={styles.btn} onPress={buttonPressed}>
+        <View>
+          <Text>Start</Text>
+        </View>
+      </TouchableOpacity>
+
+      <View>
+      <Text>Hello World, this is a Test.
+        Probably it will be an interactive story, but i'm not sure yet. Testing functionalities: Icon, Button, Scroll and Input text</Text>
+      </View>
     </SafeAreaView>
-  );
+  )
 }
-
-
--- styles.ts
+++ styles.ts
@@ -11,6 +11,21 @@ const styles = StyleSheet.create({
     
     padding: getStatusBarHeight (),
   },
+  txtname:{
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor:'#778899',
+   
+  },
+  btn:{
+    marginTop:10,
+    marginBottom:10,
+    borderWidth:1,
+    borderRadius:10,
+    borderColor:`#e6e6fa`,
+    padding:3,
+    backgroundColor: `#e6e6fa`,
+    
+  }
 })
 
 export {styles};
