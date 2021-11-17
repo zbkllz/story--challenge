12e2d3f86255d1f9ff10454ecc52c63717db62e0
Larissa <larissasousa.wrks@gmail.com>
2021-11-17 16:08:45 -0400

    :poop: stuff i really dont know but im trying to

--- App.tsx
+++ App.tsx
@@ -1,44 +1,68 @@
 import React, { useState } from 'react'
 import {
+  ScrollView,
   TextInput,
   SafeAreaView,
   View,
   Text,
-  TouchableOpacity
+  TouchableOpacity,
+  Linking,
+  Image
 } from 'react-native'
+import Svg from 'react-native-svg'
 import { StatusBar } from 'expo-status-bar'
 import { styles } from './styles'
-
-
 
 export default function App() {
   const [text, setText] = useState('')
 
-  function buttonPressed(text: any) {
-    return text
+  function buttonPressed() {
+    return
+    //return console.log(text)
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="dark" />
 
+      <Text style={styles.intro}>
+        {`Hello, this is an educational test. In my first work, based on an Android application, I made an short intro about my goals as a dev padawan, now I thought about an interactive like experience, and for this purpose I choose two paragraphs of a D&D role playing`}{' '}
+        <Text
+          style={{ color: `black`, fontWeight: 'bold' }}
+          onPress={() =>
+            Linking.openURL(
+              'https://wotbs-4e.obsidianportal.com/wikis/vignette-one'
+            )
+          }
+        >
+          fan fiction
+        </Text>
+      </Text>
+
       <TextInput
-        style={styles.txtname}
-        placeholder="Insert your name here"
+        style={styles.txtsub}
+        placeholder="Name your hero!"
         value={text}
         onChangeText={text => setText(text)}
       />
 
       <TouchableOpacity style={styles.btn} onPress={buttonPressed}>
         <View>
-          <Text>Start</Text>
+          <Text style={{ color: `white`, fontWeight: 'bold' }}>Start</Text>
         </View>
       </TouchableOpacity>
 
-      <View>
-      <Text>Hello World, this is a Test.
-        Probably it will be an interactive story, but i'm not sure yet. Testing functionalities: Icon, Button, Scroll and Input text</Text>
+      <View style={styles.fic}>
+        <Image style={styles.pic} source={require('./assets/rpgmage.png')} />
+        <Text style={styles.title}> War of The Burning Sky </Text>
       </View>
+
+      <ScrollView style={styles.fic}>
+        
+        <Text style={styles.rpg}>
+          {`"As ${text} go about his day, the city seems on edge, as if the citizens have taken a deep breath and are waiting to exhale. Most folk walk around with their heads down, murmuring to themselves. Even the jovial public houses have turned dour and depressing. In one of the lower-class neighborhoods near the West Gate, Viv Finner and her husband Trehan, proprietors of the Poison Apple Pub, are frightened.\nEveryone knew Trehan was a magus. They knew it as much from the twinkle in his eye and his perpetual smirk as they did from the fact that he could put a rowdy customer to sleep with a handful of dust. But no one seemed to mind. Most Gate Passers didn’t trust magi as a group, but just about everybody who knew Trehan Finner liked him.\n\nWith ${text}'s hands cupping a warm mug of cider, he look up at Trehan and smile. He returns the smile and winks, carefully wiping down the bar with a stained rag. He leans in and mumbles something to ${text} and his heart sinks."`}
+        </Text>
+      </ScrollView>
     </SafeAreaView>
   )
 }
--- styles.ts
+++ styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet} from 'react-native';
 import { getStatusBarHeight} from 'react-native-iphone-x-helper';
 
 
@@ -7,25 +7,69 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    
     padding: getStatusBarHeight (),
   },
-  txtname:{
+  pic:{
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    marginTop:2,
+    marginBottom:2,
+    marginLeft:100,
+    borderRadius:10,
+  },
+  intro:{
+    textAlign:'center',
+    fontSize:11,
+    backgroundColor:'#f5f5f5',
+    marginTop:10,
+    borderRadius:5, 
+    marginBottom:2,
+    marginRight:1,
+    marginLeft:1, 
+  },
+  txtsub:{
+    alignItems: 'center',
+    textAlign:'center',
+    marginRight:30,
+    marginLeft:30,
+    marginTop:10,
     borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor:'#778899',
-   
+    borderBottomColor:'#778899',     
   },
   btn:{
     marginTop:10,
     marginBottom:10,
-    borderWidth:1,
+    marginRight:80,
+    marginLeft:80,
+    borderWidth:3,
     borderRadius:10,
-    borderColor:`#e6e6fa`,
+    borderColor:`#778899`,
     padding:3,
-    backgroundColor: `#e6e6fa`,
+    backgroundColor:`#778899`,
+    alignItems: 'center',
     
-  }
+  },
+  fic:{
+    marginTop:10,
+    flexDirection: 'column',
+  },
+  title:{
+    color: `black`, 
+    fontWeight: 'bold', 
+    textAlign:'center',
+    marginTop:10,
+    marginBottom:10,
+    fontSize:18,
+  },
+  rpg:{
+    color: `black`, 
+    marginTop:10,
+    marginBottom:10,
+    fontSize:16,
+    textAlign:'justify',
+  },
+
 })
 
 export {styles};
